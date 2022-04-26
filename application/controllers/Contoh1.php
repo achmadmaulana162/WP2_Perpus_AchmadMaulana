62ac619cf537a6dc40895ed52329cee4ee40d4a7
achmadmaulana162 <achmadmaulana162@gmail.com>
2022-04-26 22:40:42 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama saya Achmad Maulana Saya tingal didaerah Jagakarsa, saya suka olahraga yaitu bulutangkis";
+	}
+}
